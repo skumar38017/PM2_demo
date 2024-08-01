87c1ce88e66a9205e5c63806ffb4331e4b57a6c9
skumar <skumar38017@gmail.com>
2024-08-01 18:38:10 +0530

    ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,21 @@ module.exports = {
       {
         name: "container0",
         script: "docker",
-        args: "run --name container1 -p 3000:3000 app-js-3000:latest"
+        args: "run --name container0 -d -p 3000:3000 app-js-3000:latest",
+        // Optional: Specify other settings like environment variables
+        // env: {
+        //   NODE_ENV: "production"
+        // }
       },
       {
         name: "container1",
         script: "docker",
-        args: "run --name container1 -p 3001:3001 app-js-3001:latest"
-      },
+        args: "run --name container1 -d -p 3001:3001 app-js-3001:latest",
+        // Optional: Specify other settings like environment variables
+        // env: {
+        //   NODE_ENV: "production"
+        // }
+      }
       // Add more containers as needed
     ]
   };
